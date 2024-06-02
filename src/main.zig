d551121d049032c836aed2da7dd28abef7c9dcc6
Casper Neo <cneo@google.com>
2024-06-02 00:56:20 -0400

    Build skeleton of a game loop.
    For now, we can render a square moving along.

--- src/main.zig
+++ src/main.zig
@@ -4,31 +4,116 @@ const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const TICK_TIME = 50;
 
-    if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_EVENTS | c.SDL_INIT_AUDIO) != 0) {
-        c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
-        return;
+const Rect = struct {
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
+    rgb: [3]u8,
+};
+
+const SdlContext = struct {
+    window: *c.SDL_Window,
+    surface: *c.SDL_Surface,
+    renderer: *c.SDL_Renderer,
+
+    pub fn init(title: [*c]const u8) SdlContext {
+        if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) < 0) {
+            c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
+        }
+        var self = SdlContext{
+            .window = undefined,
+            .surface = undefined,
+            .renderer = undefined,
+        };
+        const w = c.SDL_CreateWindow(
+            title,
+            c.SDL_WINDOWPOS_CENTERED,
+            c.SDL_WINDOWPOS_CENTERED,
+            680,
+            480,
+            0,
+        );
+        if (w == null) {
+            c.SDL_Log("Failed to create window %s", c.SDL_GetError());
+        }
+        self.window = w.?;
+        self.surface = c.SDL_GetWindowSurface(self.window).?;
+        self.renderer = c.SDL_GetRenderer(self.window).?;
+        return self;
     }
-    defer c.SDL_Quit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    pub fn destroy(self: SdlContext) void {
+        c.SDL_DestroyWindow(self.window);
+        c.SDL_Quit();
+    }
+    pub fn clear_screen(self: SdlContext) void {
+        // TODO: Probably should check these error codes...
+        _ = c.SDL_SetRenderDrawColor(self.renderer, 0, 0, 0, 128);
+        _ = c.SDL_RenderClear(self.renderer);
+    }
+    pub fn draw_rect(self: SdlContext, rect: Rect) void {
+        _ = c.SDL_SetRenderDrawColor(self.renderer, rect.rgb[0], rect.rgb[1], rect.rgb[2], 255);
+        const sdl_rect = c.SDL_Rect{
+            .x = @intCast(rect.x),
+            .y = @intCast(rect.y),
+            .w = @intCast(rect.width),
+            .h = @intCast(rect.height),
+        };
+        _ = c.SDL_RenderFillRect(self.renderer, &sdl_rect);
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    pub fn present(self: SdlContext) void {
+        _ = c.SDL_RenderPresent(self.renderer);
+    }
+};
 
-    try bw.flush(); // don't forget to flush!
-}
+const GameState = struct {
+    x: u32,
+    y: u32,
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    fn update(self: *GameState) void {
+        self.x = @mod(self.x + 1, 480);
+        self.y = @mod(self.y + 1, 360);
+    }
+
+    fn draw(self: GameState, sdl: SdlContext) void {
+        sdl.draw_rect(Rect{
+            .x = self.x,
+            .y = self.y,
+            .width = 100,
+            .height = 100,
+            .rgb = .{ 200, 100, 50 },
+        });
+    }
+};
+
+pub fn main() !void {
+    const sdl = SdlContext.init("Tetris Sands");
+    defer sdl.destroy();
+
+    var game_state = GameState{ .x = 100, .y = 100 };
+    var event = c.SDL_Event{ .type = 0 };
+    var next_time = c.SDL_GetTicks64() + TICK_TIME;
+    game_loop: while (true) {
+        // First, handle events...
+        while (c.SDL_PollEvent(&event) != 0) {
+            if (event.type == c.SDL_QUIT) break :game_loop;
+        }
+
+        game_state.update();
+        sdl.clear_screen();
+        game_state.draw(sdl);
+        sdl.present();
+
+        // Sleep until next frame.
+        const now = c.SDL_GetTicks64();
+        if (now < next_time) {
+            const remaining = next_time - now;
+            c.SDL_Delay(@intCast(remaining));
+        }
+        next_time += TICK_TIME;
+    }
 }
